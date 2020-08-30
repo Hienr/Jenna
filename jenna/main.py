d5c7feb5b831671a784410900704ac60d1025cf7
Ray <raymondhien16@gmail.com>
2020-08-30 15:30:04 -0700

    player class defined

--- jenna/main.py
+++ jenna/main.py
@@ -1,22 +1,8 @@
-import pafy
-import youtube_dl
-import vlc
+import player
+import time
 url = "https://www.youtube.com/watch?v=gFFOXwniVKw"
 
-video = pafy.new(url)                                                                                                                       
-audiostream = video.getbestaudio()
+audiostream = player.Player() 
 
-player = vlc.MediaPlayer(audiostream.url)
-# player = vlc.MediaPlayer("F:\Music\한국 음악\Cinderella Time - NC.A.mp3")
-player.play()
-
-print(video.title)
-print(video.duration)
-# print(video.rating)
-# print(video.author)
-# print(video.length)
-# print(video.keywords)
-print(video.thumb)
-# print(video.videoid)
-# print(video.viewcount)
-while True : pass
+audiostream._setVideo(url)
+audiostream._play()
