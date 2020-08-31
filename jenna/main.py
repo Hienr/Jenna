41c24ae29138486cb252cb5f7d19bb4c106daab6
Ray <raymondhien16@gmail.com>
2020-08-30 18:09:23 -0700

    added player thread to accept jobs from queue

--- jenna/main.py
+++ jenna/main.py
@@ -1,8 +1,19 @@
 import player
 import time
+import queue
+import threading
+from collections import namedtuple 
+from player import JobTypes
+Job = namedtuple('Job', 'id url')
+
 url = "https://www.youtube.com/watch?v=gFFOXwniVKw"
 
-audiostream = player.Player() 
+player_thread = player.PlayerThread()
 
-audiostream._setVideo(url)
-audiostream._play()
+while True:
+    action = int(input("Action: "))
+
+    if (action == JobTypes['ADD']):
+        player.enqueue_job((action, url))
+    else:
+        player.enqueue_job((action, None))
